d5d7acfffe77f2bbac6628cf383f6d7c4002784d
Md Jubaer Rahman <mdjubairrahman62782@gmail.com>
2026-03-29 13:05:53 +0600

    update

--- app/api/signin/route.ts
+++ app/api/signin/route.ts
@@ -1,1 +1,9 @@
 // need for mobile app
+
+import { NextResponse } from "next/server";
+
+
+export async function POST(request: Request) {
+
+    return NextResponse.json({ success: true, message: "Sign-in endpoint is under construction" }, { status: 200 });
+}       
